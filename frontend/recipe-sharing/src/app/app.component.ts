ebb074c22a87a579254eeacde47691621c28f92a
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-06-06 22:24:48 +0200

    Frontend footer and navbar components

--- frontend/recipe-sharing/src/app/app.component.ts
+++ frontend/recipe-sharing/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    CommonModule,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
