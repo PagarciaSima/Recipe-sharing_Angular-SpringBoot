7ee46038f781d6e816a3d172ebd6829c792d8dca
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-06-07 15:28:58 +0200

    Front end create recipe form modal

--- frontend/recipe-sharing/src/app/pages/recipe-card/recipe-card.component.ts
+++ frontend/recipe-sharing/src/app/pages/recipe-card/recipe-card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss'
 })
